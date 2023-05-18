0a751b1960a7ed974a1b51a9a7fd6ce1ab19eddd
NitinNitinRaj <bnitinraj@gmail.com>
2023-05-18 14:59:20 +0530

    saving orders

--- src/main/java/com/polarbookshop/orderservice/order/domain/service/OrderService.java
+++ src/main/java/com/polarbookshop/orderservice/order/domain/service/OrderService.java
@@ -3,7 +3,9 @@ package com.polarbookshop.orderservice.order.domain.service;
 import com.polarbookshop.orderservice.order.domain.entity.Order;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface OrderService {
     public Flux<Order> getAllOrders();
+    public Mono<Order> submitOrder(String isbn, int quantity);
 }
--- src/main/java/com/polarbookshop/orderservice/order/domain/service/impl/OrderServiceImpl.java
+++ src/main/java/com/polarbookshop/orderservice/order/domain/service/impl/OrderServiceImpl.java
@@ -1,10 +1,12 @@
 package com.polarbookshop.orderservice.order.domain.service.impl;
 
 import com.polarbookshop.orderservice.order.domain.entity.Order;
+import com.polarbookshop.orderservice.order.domain.entity.OrderStatus;
 import com.polarbookshop.orderservice.order.domain.repositories.OrderRepository;
 import com.polarbookshop.orderservice.order.domain.service.OrderService;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 public class OrderServiceImpl implements OrderService {
@@ -18,5 +20,14 @@ public class OrderServiceImpl implements OrderService {
   @Override
   public Flux<Order> getAllOrders() {
     return orderRepository.findAll();
+  }
+
+  @Override
+  public Mono<Order> submitOrder(String isbn, int quantity) {
+    return Mono.just(buildRejectedOrder(isbn, quantity)).flatMap(orderRepository::save);
+  }
+
+  public static Order buildRejectedOrder(String isbn, int quantity) {
+    return Order.of(isbn, null, null, quantity, OrderStatus.REJECTED);
   }
 }
