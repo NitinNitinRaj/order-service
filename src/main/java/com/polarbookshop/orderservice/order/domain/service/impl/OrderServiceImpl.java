b652057d6b6e6b70c2f17ad819b30570bcc60b36
NitinNitinRaj <bnitinraj@gmail.com>
2023-05-19 17:36:20 +0530

    Calling BookClient to get the book info when ordered

--- src/main/java/com/polarbookshop/orderservice/order/domain/service/impl/OrderServiceImpl.java
+++ src/main/java/com/polarbookshop/orderservice/order/domain/service/impl/OrderServiceImpl.java
@@ -1,10 +1,14 @@
 package com.polarbookshop.orderservice.order.domain.service.impl;
 
+import com.polarbookshop.orderservice.book.Book;
+import com.polarbookshop.orderservice.book.BookClient;
 import com.polarbookshop.orderservice.order.domain.entity.Order;
 import com.polarbookshop.orderservice.order.domain.entity.OrderStatus;
 import com.polarbookshop.orderservice.order.domain.repositories.OrderRepository;
 import com.polarbookshop.orderservice.order.domain.service.OrderService;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -12,9 +16,14 @@ import reactor.core.publisher.Mono;
 public class OrderServiceImpl implements OrderService {
 
   private final OrderRepository orderRepository;
+  private final BookClient bookClient;
 
-  public OrderServiceImpl(OrderRepository orderRepository) {
+  public OrderServiceImpl(
+    OrderRepository orderRepository,
+    BookClient bookClient
+  ) {
     this.orderRepository = orderRepository;
+    this.bookClient = bookClient;
   }
 
   @Override
@@ -24,7 +33,36 @@ public class OrderServiceImpl implements OrderService {
 
   @Override
   public Mono<Order> submitOrder(String isbn, int quantity) {
-    return Mono.just(buildRejectedOrder(isbn, quantity)).flatMap(orderRepository::save);
+    // return Mono
+    //   .just(buildRejectedOrder(isbn, quantity))
+    //   .flatMap(orderRepository::save);
+
+    return bookClient
+      .getBookByIsbn(isbn)
+      .map(book -> buildAcceptedOrder(book, quantity))
+      .defaultIfEmpty(buildRejectedOrder(isbn, quantity))
+      .flatMap(order -> orderRepository.save(order))
+      .onErrorResume(
+        WebClientResponseException.class,
+        e -> {
+          if (e.getStatusCode() == HttpStatus.NOT_FOUND) {
+            return orderRepository.save(buildRejectedOrder(isbn, quantity));
+          } else {
+            // Handle other WebClientResponseException scenarios
+            return Mono.error(e);
+          }
+        }
+      );
+  }
+
+  public static Order buildAcceptedOrder(Book book, int quantity) {
+    return Order.of(
+      book.isbn(),
+      book.title(),
+      book.price(),
+      quantity,
+      OrderStatus.ACCEPTED
+    );
   }
 
   public static Order buildRejectedOrder(String isbn, int quantity) {
